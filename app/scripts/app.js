2d876d6a5db24dd122b9bf5dc3566b4a035ccac5
NekoPavel <neko@pavel.gg>
2025-07-24 10:49:14 +0200

    Initial upload

---
+++ app/scripts/app.js
@@ -0,0 +1,159 @@
+let client;
+app.initialized().then(
+	function (client) {
+		//If successful, register the app activated and deactivated method callback.
+		client.events.on("app.activated", onAppActivated(client));
+		//client.events.on("app.deactivated", onAppDeactivated);
+	},
+	function (error) {
+		//If unsuccessful
+		handleErr(error);
+	}
+);
+
+function onAppActivated(_client) {
+	client = _client;
+	client.events.on('app.activated', function log() {
+		//return console.log(`full page location:  app is activated 🥁`);
+	});
+	//Prepare coming Monday as a start
+	const fromDate = new Date();
+	fromDate.setDate(fromDate.getDate() + (((1 + 7 - fromDate.getDay()) % 7) || 7));
+	const fromDateString = fromDate.toISOString().slice(0, 10);
+
+	//Prepare coming Sunday as end
+	const toDate = new Date();
+	toDate.setDate(fromDate.getDate() + 6);
+	const toDateString = toDate.toISOString().slice(0, 10);
+
+	//Set date for start
+	const startDate = document.querySelector('input[id="start"]');
+	startDate.value = fromDateString;
+
+	//Set date for end
+	const endDate = document.querySelector('input[id="end"]');
+	endDate.value = toDateString;
+
+	//getEmployeeList(client);
+}
+
+function sortByDate(a, b) {
+	if (a.due_by > b.due_by) return 1;
+	else if (b.due_by > a.due_by) return -1;
+	else return 0;
+}
+
+async function getEmployeeList() {
+	try {
+		//Create table holder
+		document.getElementById('employeeTable').innerHTML = `
+		<div id="tableHeader" class="row">
+			<div class="cell">
+				Ärendenummer
+			</div>
+			<div class="cell">
+				Bolag
+			</div>
+			<div class="cell">
+				Upphämtningsställe
+			</div>
+			<div class="cell">
+				Datum
+			</div>
+			<div class="cell">
+				Anställningstyp & Namn
+			</div>
+			<div class="cell">
+				Status
+			</div>
+		</div>`;
+
+		//Read group memberships from logged in user
+		const userGroups = (await client.data.get("loggedInUser")).loggedInUser.group_ids;
+		console.log((await client.data.get("loggedInUser")).loggedInUser);
+		//Format for search query
+		const groupsString = "group_id:" + userGroups.join(" OR group_id:");
+
+		//Get tickets
+		const data = await getTickets(groupsString);
+		const parsedResponse = JSON.parse(data.response);
+		
+		let tickets = parsedResponse.tickets;
+		const ticketsCount = parsedResponse.total;
+
+		//Sort tickets by date
+		tickets = tickets.sort(sortByDate);
+
+		//Handle pagination
+		if (ticketsCount > tickets.length) {
+			document.getElementById('employeeTable').innerHTML = '<h3>Too many tickets in timespan, please narrow down your search.</h3>';
+			//get next page either by making a next page button or getting everything (bad idea)
+		}
+		//Handle empty response
+		else if (tickets.length === 0) {
+			document.getElementById('employeeTable').innerHTML = '<h3>No tickets found in selected timerange! 🎉</h3>';
+		}
+		//Show results
+		else {
+			for (const ticket in tickets) {
+				const ticketObj = tickets[ticket];
+				const words = ticketObj.subject.split('|');
+				const title = words[1].split(' - ');
+				addRow(ticketObj.id, words[0], title[0], title[1], words[2], ticketObj.status);
+			};
+		}
+
+		//Get list of upcoming tickets that are to be sent
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+function handleErr(err = 'None') {
+	console.error(`Error occured. Details:`, err);
+}
+
+async function getTickets(groups, page = 1) {
+	return await client.request.invokeTemplate('requestNewEmployeeList', {
+		context: {
+			toDate: document.getElementById("end").value,
+			fromDate: document.getElementById("start").value,
+			groups: groups,
+			page: page
+		}
+	});
+}
+
+const statusList = [
+	{},
+	{},
+	{ text: 'Open', 	colour: '#8be9fd55' },
+	{ text: 'Pending', 	colour: '#f1fa8c55' },
+	{ text: 'Resolved', colour: '#50fa7b55' },
+	{ text: 'Resolved', colour: '#50fa7b55' },
+	{ text: 'Hold', 	colour: '#f1fa8c55' }
+];
+
+function addRow(ticketId, company, pickupLocation, date, typeAndName, statusId) {
+	document.getElementById('employeeTable').innerHTML += `
+		<a class="row linkRow" target="_blank" href="https://bonniernews.freshservice.com/a/tickets/${ticketId}">
+			<div class="cell isFirstColumn">
+				#SR-${ticketId}
+			</div>
+			<div class="cell">
+				${company}
+			</div>
+			<div class="cell">
+				${pickupLocation}
+			</div>
+			<div class="cell">
+				${date}
+			</div>
+			<div class="cell">
+				${typeAndName}
+			</div>
+			<div id="statusCell" class="cell" style="background-color:${statusList[statusId].colour}">
+				${statusList[statusId].text}
+			</div>
+		</a>`;
+}
